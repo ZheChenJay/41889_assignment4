8b738b613a93d566fd3891edb808b08a94782a94
ZheChenJay <105184225+ZheChenJay@users.noreply.github.com>
2022-05-09 16:42:41 +0800

    Add files via upload

---
+++ Ios_ass4/Ios_ass4/ProductVC.swift
@@ -0,0 +1,23 @@
+//
+//  ProductVC.swift
+//  Ios_ass4
+//
+//  Created by Chanzhe on 2022/5/9.
+//
+
+import UIKit
+
+class ProductVC: UIViewController {
+    
+    private var tableView:UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+        view.backgroundColor = .red
+        
+        tableView = 
+    }
+
+
+}
+
