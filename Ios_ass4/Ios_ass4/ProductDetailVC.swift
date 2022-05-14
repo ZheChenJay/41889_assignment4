65c63101a3be5f2a7f2bf759d9afc997198f611c
ZheChenJay <zhe.chen-3@student.uts.edu.au>
2022-05-14 21:32:55 +0800

    iOS

--- Ios_ass4/Ios_ass4/ProductDetailVC.swift
+++ Ios_ass4/Ios_ass4/ProductDetailVC.swift
@@ -11,6 +11,8 @@ class ProductDetailVC: UIViewController {
 
     private let product: Product
     
+    private var tableView: UITableView!
+    
     init(product: Product){
         self.product = product
         super.init(nibName: nil, bundle: nil)
@@ -23,7 +25,24 @@ class ProductDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        
     }
 
 }
