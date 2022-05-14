e82e117c2be52e215088420dc0385b224721f1e1
YueyangHong <yz2719526634@gmail.com>
2022-05-14 21:57:35 +0800

    ios

---
+++ Ios_ass4/ProductDetailVC.swift
@@ -0,0 +1,49 @@
+//
+//  ProductDetailVC.swift
+//  
+//
+//  Created by HongYueyang on 2022/5/14.
+//
+
+
+import UIKit
+
+class ProductDetailVC: UIViewController {
+
+    private let product: Product
+    
+    private var tableView: UITableView!
+    
+    init(product: Product){
+        self.product = product
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+        
+        
+    }
+
+}
