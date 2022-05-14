f3ae402fd48b43c5e35a9e1d377dab7baae3d380
Longlei-Guo <105506336+Longlei-Guo@users.noreply.github.com>
2022-05-14 16:43:40 +0800

    Update NetworkAPI.swift

--- Ios_ass4/Ios_ass4/NetworkAPI.swift
+++ Ios_ass4/Ios_ass4/NetworkAPI.swift
@@ -6,19 +6,19 @@
 //
 
 import UIKit
-import Alamofire
+import Alamofire //network third-party library
 
 private let BaseUrl = "https://raw.githubusercontent.com/ZheChenJay/API-product/main/API/Shopping/"
 
 class NetworkAPI {
     static func homeProductList(completion: @escaping (Result<[Product], Error>) -> Void){
-        AF.request(BaseUrl + "iphone_1.json").responseData { response in
+        AF.request(BaseUrl + "iphone_1.json").responseData { response in //request data from URL
             switch response.result {
             case let .success(data):
                 if let list = try? JSONDecoder().decode([Product].self, from: data){
                     completion(.success(list))
                 }else{
-                    let error = NSError(domain: "NetworkAPIDomain", code: 0, userInfo: [NSLocalizedDescriptionKey:"decode error"])
+                    let error = NSError(domain: "NetworkAPIDomain", code: 0, userInfo: [NSLocalizedDescriptionKey:"decode error"]) //error information
                     completion(.failure(error))
                 }
             case let .failure(error):
