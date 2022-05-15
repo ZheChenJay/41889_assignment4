f0b76ec277d2f4eae29ebea54d0948a32b18c422
RebornHO <44311154+RebornHO@users.noreply.github.com>
2022-05-15 13:50:39 +0800

    Update CycleView.swift

--- Ios_ass4/Ios_ass4/CycleView.swift
+++ Ios_ass4/Ios_ass4/CycleView.swift
@@ -11,6 +11,8 @@ class CycleView: UIView {
     
     private var image: [String] = []
     private var collectionView: UICollectionView!
+    private var pageControl: UIPageControl!
+    private var multiplier = 3
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -39,6 +41,17 @@ class CycleView: UIView {
             collectionView.topAnchor.constraint(equalTo: topAnchor),
             collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
         ])
+                    
+        pageControl = UIPageControl()
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.currentPageIndicatorTintColor = .systemBlue
+        pageControl.pageIndicatorTintColor = .systemGray
+        addSubview(pageControl)
+        
+        NSLayoutConstraint.activate([
+            pageControl.centerXAnchor.constraint(equalTo: centerXAnchor),
+            pageControl.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
+        ])
     }
     
     required init?(coder: NSCoder) {
@@ -48,18 +61,27 @@ class CycleView: UIView {
     func setImage(_ images: [String]){
         self.image = images
         collectionView.reloadData()
+        collectionView.layoutIfNeeded()
+        pageControl.numberOfPages = image.count
+        scrollToCenter()
+    }
+    
+    private func scrollToCenter(){
+        collectionView.scrollToItem(at: IndexPath(item: multiplier / 2 * image.count, section: 0),
+                                    at: .centeredVertically, animated: false)
+
     }
 }
 
 extension CycleView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        image.count
+        image.count * multiplier
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CycleViewCell.description(), for: indexPath) as! CycleViewCell
         cell.imageView.image = UIImage(systemName: "heart.fill")
-        cell.imageView.sh_setImage(name: image[indexPath.item])
+        cell.imageView.sh_setImage(name: image[indexPath.item % image.count])
         return cell
     }
 }
@@ -68,7 +90,17 @@ extension CycleView:UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         collectionView.bounds.size
     }
+        
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let index = Int((scrollView.contentOffset.x + 5) / scrollView.bounds.width) % image.count
+        pageControl.currentPage = index
+        
+        if index == 0 {
+            scrollToCenter()
+        }
+    }
 }
+
 
 private class CycleViewCell: UICollectionViewCell {
     
