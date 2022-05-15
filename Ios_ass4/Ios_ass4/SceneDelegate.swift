b3258942db7cbca4c445f4bae0aeb7de6adbd02b
RebornHO <44311154+RebornHO@users.noreply.github.com>
2022-05-15 13:44:10 +0800

    Update SceneDelegate.swift

--- Ios_ass4/Ios_ass4/SceneDelegate.swift
+++ Ios_ass4/Ios_ass4/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         //show the first page of product
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UINavigationController(rootViewController: ProductListVC())
+        window.rootViewController = TabBarControler()
         window.makeKeyAndVisible()
         self.window = window
     }
